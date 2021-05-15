b409ea7f789ceb5b8dd8f90b2013e7648bb10550
akash <akash@MSI>
2021-05-15 06:59:33 +0530

    Find Maximum Number in given Flots

--- src/main/java/MaxTest.java
+++ src/main/java/MaxTest.java
@@ -1,12 +1,34 @@
 public class MaxTest {
     public int findMaxNumber(Integer one, Integer two, Integer three) {
-        Integer maxNumber = one;
-        if (two.compareTo(maxNumber) > 0) {
-            maxNumber = two;
+        Integer maxNum = one;
+        if (two.compareTo(maxNum) > 0) {
+            maxNum = two;
         }
-        if (three.compareTo(maxNumber) > 0) {
-            maxNumber = three;
+        if (three.compareTo(maxNum) > 0) {
+            maxNum = three;
         }
-        return maxNumber;
+        return maxNum;
+    }
+
+    public float findFloatMaxNumber(Float one, Float two, Float three) {
+        Float maxNum = one;
+        if (two.compareTo(maxNum) > 0) {
+            maxNum = two;
+        }
+        if (three.compareTo(maxNum) > 0) {
+            maxNum = three;
+        }
+        return maxNum;
+    }
+
+    public String findStringMaxNumber(String stringOne, String stringTwo, String stringThree) {
+        String maxNum = stringOne;
+        if (stringTwo.compareTo(maxNum) > 0) {
+            maxNum = stringTwo;
+        }
+        if (stringThree.compareTo(maxNum) < 0) {
+            maxNum = stringThree;
+        }
+        return maxNum;
     }
 }
--- src/test/java/FindmaxTest.java
+++ src/test/java/FindmaxTest.java
@@ -17,4 +17,21 @@ class FindMaxTest {
     void ifNumberThreeIsMaxReturnTrue() {
         Assertions.assertEquals(22, findMax.findMaxNumber(1, 5, 22));
     }
+
+    @Test
+    void ifFloatNumberOneIsMaxReturnTrue() {
+        Assertions.assertEquals(10.0, findMax.findFloatMaxNumber(10.0F, 5.0F, 2.5F));
+    }
+
+    @Test
+    void ifFloatNumberTwoIsMaxReturnTrue() {
+        Assertions.assertEquals(10.0, findMax.findFloatMaxNumber(5.0F, 10.0F, 2.5F));
+    }
+
+    @Test
+    void ifFloatNumberThreeIsMaxReturnTrue() {
+        Assertions.assertEquals(10.0, findMax.findFloatMaxNumber(2.5F, 5.0F, 10.0F));
+    }
 }
+
+
